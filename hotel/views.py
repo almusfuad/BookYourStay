ef7ec2dad703db6a3fdda9c2a7428c1b78f6f67a
almusfuad <almusfuad@gmail.com>
2024-01-25 23:50:34 +0600

    change some design

--- hotel/views.py
+++ hotel/views.py
@@ -34,14 +34,17 @@ class HotelDetailsView(DetailView):
             context = super().get_context_data(**kwargs)
             hotel = self.get_object()
             
+            
             if self.request.user.is_authenticated:
                   student = self.request.user.student
                   context['user_has_booked'] = Booking.objects.filter(hotel=hotel, student = student).exists()   
                   context['review_owner'] = Review.objects.filter(hotel=hotel, student=student).exists()     # with exists() we get the boolean value
-                  context['reviews'] = Review.objects.filter(hotel = hotel)
-                  context['edit_review'] = Review.objects.filter(hotel=hotel, student=student)
+                  
+                  context['edit_review'] = Review.objects.filter(hotel=hotel, student=student).values('rating', 'review')
+                  print(context['edit_review'])
                   context['review_form'] = ReviewForm(user = self.request.user)
-                  return context
+            context['reviews'] = Review.objects.filter(hotel = hotel)
+            return context
       
       def post(self, request, *args, **kwargs):
             review_form = ReviewForm(user = request.user, data = self.request.POST, )
